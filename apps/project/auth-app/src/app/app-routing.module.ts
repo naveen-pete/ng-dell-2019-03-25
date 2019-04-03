9965e7a7334aba777b7407b24e9b5671cd5842ae
naveen-pete <naveen.shenoy.blr@gmail.com>
2019-04-03 17:47:16 +0530

    commit code for guard in auth app

--- apps/project/auth-app/src/app/app-routing.module.ts
+++ apps/project/auth-app/src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProductsComponent } from './products/products.component';
 
+import { AuthGuard } from './services/auth.guard';
+
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'products', component: ProductsComponent },
+  { path: 'products', component: ProductsComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '' }
 ];
 
